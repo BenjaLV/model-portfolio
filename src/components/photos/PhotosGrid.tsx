9afd8b9e2cbc59cb5de18f159e8db3189bf34ef8
BenjaLV <benjalavalla8@gmail.com>
2023-12-29 22:14:44 -0300

    realese 1.0.2

--- src/components/photos/PhotosGrid.tsx
+++ src/components/photos/PhotosGrid.tsx
@@ -2,22 +2,27 @@ import { Photo } from '@/interfaces/photos-interfaces'
 import React from 'react'
 import PhotoGridItem from './PhotoGridItem';
 
+
 interface Props {
     photos: Photo[];
 }
 
 function PhotosGrid({ photos }: Props) {
+
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2'>
-            {
-                photos.map(photo => (
-                    <div className='my-1 mx-1'>
-                        <PhotoGridItem key={photo.slug} photo={photo} />
+            {photos.map((photo) => {
+                return (
+                    <div className='my-1 mx-1' key={photo.slug}>
+                        <PhotoGridItem photo={photo} />
                     </div>
-                ))
-            }
+                );
+            })}
         </div>
     )
 }
+
+
+
 
 export default PhotosGrid
